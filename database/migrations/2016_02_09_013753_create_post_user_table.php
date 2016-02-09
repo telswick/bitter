e5fa53ea7d9d189881ac190ba44a71e92d567668
Traci Elswick <tracielswick@Tracis-MacBook-Air.local>
2016-02-08 19:46:11 -0600

    Add table migrations

---
+++ database/migrations/2016_02_09_013753_create_post_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post_user');
+    }
+}
