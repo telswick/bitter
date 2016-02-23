9949dc4220227bf8ae537e735c3fea0f6e8a5e5e
Traci Elswick <tracielswick@Tracis-MacBook-Air.local>
2016-02-23 15:45:56 -0600

    Make RESTful PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -16,28 +16,27 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Post::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
+     * Posts have user_id and text
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $post = new \App\Post;
+
+        $post->user_id = Auth::user()->id;
+        $post->text = $request->text;
+
+        $post->save();
+
+        return $post;
     }
 
     /**
@@ -48,22 +47,16 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        return \App\Post::with([
+            'user'
+        ])->find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
+     * Posts have user_id and text
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -71,7 +64,14 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = \App\Post::find($id);
+
+        $post->user_id = Auth::user()->id;
+        $post->text = $request->text;
+
+        $post->save();
+
+        return $post;
     }
 
     /**
@@ -82,6 +82,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = App\Post::find($id);
+        $post->delete();
+
+        return $post;
     }
 }
