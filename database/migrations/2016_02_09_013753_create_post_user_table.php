6a7747c0a29360e3b0483230f54055b12575bb00
Traci Elswick <telswick@austin.rr.com>
2016-03-27 12:03:39 -0500

    Add index to post and user

--- database/migrations/2016_02_09_013753_create_post_user_table.php
+++ database/migrations/2016_02_09_013753_create_post_user_table.php
@@ -15,8 +15,8 @@ class CreatePostUserTable extends Migration
         Schema::create('post_user', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->integer('user_id')->unsigned();
-            $table->integer('post_id')->unsigned();
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('post_id')->unsigned()->index();
         });
     }
 
