12fbc3477807c6dfd3ff8ee5667150fc309b32b1
Traci Elswick <telswick@austin.rr.com>
2016-03-27 20:46:37 -0500

    Add HomeView and PostsListView

--- public/js/app.js
+++ public/js/app.js
@@ -13,13 +13,64 @@
 		idAttribute: 'id',
 	});
 
-	
-
-	
-
 	var PostsCollection = Backbone.Collection.extend({
 		url: '/api/posts/',
 		model: PostModel
 	});
 
-	
+	// Add Views for Home and Posts	
+
+var HomeView = Backbone.View.extend({
+		el:'\
+			<div class="container">\
+				<div class="row">\
+					<div class="three columns"></div>\
+					<div class="six columns">\
+						<div class="row">\
+							<div class="twelve columns"></div>\
+						</div>\
+						<div class="row">\
+							<div class="twelve columns"></div>\
+						</div>\
+					</div>\
+					<div class="three columns" id="all-posts"></div>\
+				</div>\
+			</div>\
+		',
+
+		render: function() {
+			var posts = new PostsCollection();
+			posts.fetch();
+			var postsListView = new PostsListView({ 
+				collection: posts
+			});
+			this.$el.find('#all-posts').html(postsListView.render().el);
+
+			return this;
+		}
+	});
+
+	var PostsListView = Backbone.View.extend({
+		el: '<ul></ul>',
+
+		template: _.template('\
+			<% posts.each(function(post) { %>\
+				<li><a href="#"><%= post.get("text") %></a></li>\
+			<% }) %>\
+		'),
+
+		initialize: function() {
+			this.listenTo(this.collection, 'update', this.render);
+		},
+
+		render: function() {
+			this.$el.html(this.template({ posts: this.collection }));
+			return this;
+		}
+	})
+
+
+	var homeView = new HomeView();
+	$('#content').html(homeView.render().el);
+
+// })
