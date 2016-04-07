114d942379840380e359fa9c9fd952f67deefd41
Traci Elswick <telswick@austin.rr.com>
2016-04-06 23:07:18 -0500

    Rearrange HomeView

--- public/src/js/views/HomeView.js
+++ public/src/js/views/HomeView.js
@@ -5,16 +5,13 @@ var HomeView = Backbone.View.extend({
 		el:'\
 			<div class="container">\
 				<div class="row">\
-					<div class="three columns"></div>\
 					<div class="six columns">\
 						<div class="row">\
-							<div class="twelve columns"></div>\
-						</div>\
-						<div class="row">\
-							<div class="twelve columns"></div>\
+							<div class="twelve columns" id="all-posts"></div>\
 						</div>\
 					</div>\
-					<div class="three columns" id="all-posts"></div>\
+					<div class="three columns"></div>\
+					<div class="three columns"></div>\
 				</div>\
 			</div>\
 		',
